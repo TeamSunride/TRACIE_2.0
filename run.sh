d3efcec02249eb576f083e42103fb48e9ef381d3
Starman <30315137+Starman3787@users.noreply.github.com>
2025-04-29 19:19:24 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,12 @@ node server.js &
 cd OpenLayerMap
 npx vite build
 cd ..
-python TRACIE-GUI.py &
+if command -v python3 &>/dev/null; then
+    python3 TRACIE-GUI.py &
+elif command -v python &>/dev/null; then
+    python TRACIE-GUI.py &
+else
+    echo "Error: Python is not installed." >&2
+    exit 1
+fi
 wait
